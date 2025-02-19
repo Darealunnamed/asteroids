30a2269d0c4f264d98469fbb18db5ce9224dd186
Darealunnamed <darealunnamed@gmx.at>
2025-02-19 12:15:07 +0100

    add player and player movement logic

--- main.py
+++ main.py
@@ -8,7 +8,15 @@ def main():
   pygame.init()
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
   clock = pygame.time.Clock()
+  
+
+  updateable = pygame.sprite.Group()
+  drawable = pygame.sprite.Group()
+
+  Player.containers = (updateable, drawable)
+
   player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
   dt = 0
   
   while True:
@@ -17,7 +25,9 @@ def main():
         return
       
     screen.fill((0, 0, 0))
-    player.draw(screen)
+    updateable.update(dt)
+    for obj in drawable:
+      obj.draw(screen)
     pygame.display.flip()
     
     dt = clock.tick(60) / 1000
